f11a31bafed02f5b55c57e93ed626f4ca47efb7d
izzdanial <41255403+izzdanial99@users.noreply.github.com>
2022-03-14 21:23:53 +0800

    Create BankDemoTest.java

---
+++ Qn2/BankDemoTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class BankDemoTest{
+
+    public static void main(String[] args) {
+        Scanner scan =  new Scanner(System.in);
+        CheckingAccount cust = new CheckingAccount(1);
+        System.out.print("Enter amount to deposit: $");
+        double depositAmt = scan.nextDouble();
+        cust.deposit(depositAmt);
+
+        try {
+            System.out.print("Enter amount to withdraw: $");
+            double withdrawAmt = scan.nextDouble();
+            cust.withdraw(withdrawAmt);
+            System.out.printf("The balance after withdraw is: $%.2f",cust.getbalance());
+        } catch (InsufficientFundsException e) {
+            //TODO: handle exception
+        }
+        scan.close();
+    }
+}
