6262b6dff486e4320fb0f8dd0eb15c7f8c605b48
izzdanial <41255403+izzdanial99@users.noreply.github.com>
2022-03-15 09:58:33 +0800

    Update BankDemoTest.java

--- Qn2/BankDemoTest.java
+++ Qn2/BankDemoTest.java
@@ -4,11 +4,11 @@ public class BankDemoTest{
     public static void main(String[] args) {
         Scanner scan =  new Scanner(System.in);
         CheckingAccount cust = new CheckingAccount(1);
-        System.out.print("Enter amount to deposit: $");
-        double depositAmt = scan.nextDouble();
-        cust.deposit(depositAmt);
 
         try {
+            System.out.print("Enter amount to deposit: $");
+            double depositAmt = scan.nextDouble();
+            cust.deposit(depositAmt);
             System.out.print("Enter amount to withdraw: $");
             double withdrawAmt = scan.nextDouble();
             cust.withdraw(withdrawAmt);
